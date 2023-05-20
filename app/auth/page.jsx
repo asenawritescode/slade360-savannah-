f1198c1973d470fec30bc29dda70a2210b52fb60
DevLaukey <devlaukeymwaura@gmail.com>
2023-05-20 15:43:49 +0300

    Done

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,40 +1,64 @@
 "use client";
 
-import axios from "axios"
+import axios from "axios";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const page = () => {
-    const [memberNumber, setMemberNumber] = useState("DEMO/001");
-    const [insuranceCompany, setInsuranceCompany] = useState(457);
+  const [memberNumber, setMemberNumber] = useState("DEMO/001");
+  const [insuranceCompany, setInsuranceCompany] = useState(457);
 
-    const url =
-      "https://provider-edi-api.multitenant.slade360.co.ke/v1/beneficiaries/member_eligibility/";
+  const [token, setToken] = useState(null);
 
+  const BASEURL = "https://accounts.multitenant.slade360.co.ke/";
+  async function makeRequest(endpoint, type, payload, headers) {
+    // switch between the type of request
 
-    const headers = {
-      Accept: "*/*",
-      Authorization: "Login to authorize",
-      "Content-Type": "application/json",
-    };
-    const getUser =  async() => {
-        try {
-            const response = await axios.post(
-                url,
-                {
-                    memberNumber,
-                    insuranceCompany,
-                }
-            );
-            console.log(response.data);
-        } catch (error) {
+    await fetch(BASEURL + endpoint, {
+      method: type,
+      body: payload,
+      headers: headers,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((r) => setToken(r.access_token))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const string =
+    "grant_type=password&client_id=XdIjJgLQBOt8GCAti5GE9413y5BsR2V2IzybSj5q&client_secret=kC0N0LHwYjvv60QmsWMiPv7J7ZZoSHsb7cdLf9pgsmxInGXcBWj3Gw6KKAU9GRqO6JKpiO4y9pSwybo9SSH3chdq31jYU4V0NEhDIztGfiYgeSOG2NJorWl2ENDG0y8f&username=angelmuttai@gmail.com&password=A1997Gaa!";
+
+  useEffect(() => {
+    makeRequest("oauth2/token/", "POST", string, {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    });
+  }, []);
+
+  const url = `https://provider-edi-api.multitenant.slade360.co.ke/v1/beneficiaries/member_eligibility/?member_number=${memberNumber}&payer_slade_code=${insuranceCompany}`;
+
+  const headers = {
+    Accept: "*/*",
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+
+  const payload = {};
+  const getUser = async () => {
+    console.log(memberNumber, insuranceCompany);
+    memberNumber && insuranceCompany
+      ? fetch(url, { method: "GET", headers })
+          .then((response) =>{ return response.json()})
+          .then((data) => console.log(data))
+          .catch((error) => {
             console.log(error);
-        }
-    }
+          })
+      : alert("Fill in the data");
+  };
 
-
-
- 
   return (
     <section>
       <div class="container h-full px-6 py-24">
@@ -42,9 +66,9 @@ const page = () => {
           <div class="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
             <Image
               src="/auth.svg"
-                          class="w-full"
-                          width={500}
-                            height={500}
+              class="w-full"
+              width={500}
+              height={500}
               alt="Phone image"
             />
           </div>
@@ -78,6 +102,7 @@ const page = () => {
                       <input
                         type="text"
                         name="member_number"
+                        onChange={(e) => setMemberNumber(e.target.value)}
                         id="member_number"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="23UUAHD62"
@@ -86,8 +111,8 @@ const page = () => {
                     </div>
                     <div className="relative w-full lg:max-w-sm">
                       <select
-                        className="w-full p-2.5 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600 py-2 text-sm text-gray-700 dark:text-gray-200"
-                        onChange={(e) => console.log(e.target.value)}
+                        className="w-full p-2.5  border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600 py-2 text-sm text-gray-700 dark:text-gray-200"
+                        onChange={(e) => setInsuranceCompany(e.target.value)}
                       >
                         <option
                           class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
@@ -119,20 +144,25 @@ const page = () => {
                         >
                           Minet Insurance Brokers Limited{" "}
                         </option>
-                        <option class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" value="2023">
+                        <option
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          value="2023"
+                        >
                           Savannah Informatics Insurance Scheme{" "}
                         </option>
                         <option class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                           GNRSH Insurance Scheme{" "}
                         </option>
-                       
                       </select>
                     </div>
 
                     <button
                       type="submit"
-                                          class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                        onClick={getUser}
+                      class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        getUser();
+                      }}
                     >
                       Sign in
                     </button>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
-import Image from 'next/image'
+
 
 export default function Home() {
+
+  // makeRequest(
+  //   "oauth2/token/",
+  //   "POST",
+  //   string,
+  //   {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     "Accept": "application/json"
+  //   }
+  // )
+
   return (
   //  validate
 
